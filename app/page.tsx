8bb516bb566fdb9fd53c1a926fb3e45718ae6df7
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-12-13 10:21:25 +0800

    auth init 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/auth/sign-in")
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
+  if (session?.user) redirect("/dashboard");
+  else redirect("/auth/sign-in");
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
